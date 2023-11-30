e8c10774129e8c81f13246414a3e37a4e83939f1
mcarpent007 <60732871+mcarpent007@users.noreply.github.com>
2023-11-30 21:05:01 +0100

    Implemented xhr.upload.onprogress event handler in uploadFile function to update upload percentage and speed dynamically during the file upload process.

--- public/js/upload.js
+++ public/js/upload.js
@@ -3,21 +3,23 @@ let fileInfoDisplay;
 let speedChart; // Declare speedChart globally
 
 async function getSignedUrl(filename, contentType) {
-  const response = await fetch(
-    `http://localhost:3000/generate-signed-url?filename=${encodeURIComponent(
-      filename
-    )}&contentType=${encodeURIComponent(contentType)}`
-  );
-  if (response.ok) {
-    const data = await response.json();
-    return data.signedUrl; // This is the signed URL
-  } else {
-    throw new Error("Failed to get signed URL");
+  try {
+    const response = await fetch(`http://localhost:3000/generate-signed-url?filename=${encodeURIComponent(filename)}&contentType=${encodeURIComponent(contentType)}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data.signedUrl;
+    } else {
+      console.error('Server responded with an error:', response.status, response.statusText);
+      return null;
+    }
+  } catch (error) {
+    console.error('Failed to get signed URL:', error.message);
+    return null;
   }
 }
 
 function updateSpeedChart(chart, speed) {
-  chart.data.labels.push(""); // Add empty label for each data point
+  chart.data.labels.push(""); 
   chart.data.datasets.forEach((dataset) => {
     dataset.data.push(speed);
   });
@@ -25,36 +27,43 @@ function updateSpeedChart(chart, speed) {
 }
 
 function calculateSpeed(bytesUploaded, startTime) {
-  const duration = (new Date().getTime() - startTime) / 1000; // Time in seconds
-  return bytesUploaded / duration / 1024; // Speed in KB/s
+  const duration = (new Date().getTime() - startTime) / 1000; 
+  return bytesUploaded / duration / 1024; 
 }
 
-async function uploadFile() {
-  const fileInput = document.getElementById("file-input");
-  const file = fileInput.files[0];
-  if (!file) {
-    console.error("No file selected");
+async function uploadFile(file) {
+  const url = await getSignedUrl(file.name, file.type);
+
+  if (!url) {
+    console.error("No signed URL returned.");
     return;
   }
 
-  const signedUrl = await getSignedUrl(file.name, file.type);
   const xhr = new XMLHttpRequest();
-  const startTime = new Date().getTime();
-  document.getElementById("uploadPercentage").style.opacity = "1";
-  document.getElementById("uploadSpeed").style.opacity = "1";
-  xhr.upload.onprogress = function (event) {
-    if (event.lengthComputable) {
-      const percentComplete = (event.loaded / event.total) * 100;
-      document.getElementById(
-        "uploadPercentage"
-      ).innerText = `${percentComplete.toFixed(2)}%`;
+  xhr.open("PUT", url, true);
 
-      const speed = calculateSpeed(event.loaded, startTime);
-      document.getElementById("uploadSpeed").innerText = `${speed.toFixed(
-        2
-      )} KB/s`;
-      updateSpeedChart(speedChart, speed);
+  const startTime = new Date().getTime(); // Capture the start time for speed calculation
+
+  xhr.upload.onprogress = function(e) {
+    if (e.lengthComputable) {
+      const percentComplete = (e.loaded / e.total) * 100;
+      document.getElementById("uploadPercentage").innerText = percentComplete.toFixed(2) + "%"; // Update progress percentage
+
+      const speed = calculateSpeed(e.loaded, startTime); // Calculate upload speed
+      document.getElementById("uploadSpeed").innerText = speed.toFixed(2) + " KB/s"; // Update upload speed display
     }
+  };
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      console.log("File uploaded successfully");
+    } else {
+      console.error("Upload failed:", xhr.responseText);
+    }
+  };
+
+  xhr.onerror = function() {
+    console.error("Error during the upload process.");
   };
 
   xhr.onreadystatechange = function () {
@@ -64,11 +73,11 @@ async function uploadFile() {
     }
   };
 
-  xhr.open("PUT", signedUrl, true);
   xhr.setRequestHeader("Content-Type", file.type);
   xhr.send(file);
-  document.getElementById("speedChart").style.display = "block"; // Show the chart
+  document.getElementById("speedChart").style.display = "block"; // Show the chart, if relevant to your application
 }
+
 
 function handleDragOver(e) {
   e.preventDefault();
@@ -89,7 +98,7 @@ function resetDragDropArea() {
 function handleFileSelect(e) {
   const file = e.target.files[0];
   displayFileInfo(file);
-  document.getElementById("upload-button").disabled = !file; // Enable button only if file is selected
+  document.getElementById("upload-button").disabled = !file;
   resetUploadDisplay();
 }
 
@@ -97,7 +106,7 @@ function displayFileInfo(file) {
   if (file) {
     fileInfoDisplay.textContent = `Selected file: ${file.name}`;
   } else {
-    fileInfoDisplay.textContent = ""; // Clear the text if no file is selected
+    fileInfoDisplay.textContent = "";
   }
 }
 
@@ -109,25 +118,23 @@ function resetUploadDisplay() {
   const uploadPercentage = document.getElementById("uploadPercentage");
   const uploadSpeed = document.getElementById("uploadSpeed");
 
-  // Start fading out
   uploadPercentage.style.opacity = "0";
   uploadSpeed.style.opacity = "0";
 
-  // Wait for the fade-out transition to finish
   setTimeout(() => {
     uploadPercentage.innerText = "0%";
     uploadSpeed.innerText = "0 KB/s";
 
-    // Reset opacity for the next upload
     uploadPercentage.style.opacity = "1";
     uploadSpeed.style.opacity = "1";
-  }, 10000); // 3000ms = 3 seconds
+  }, 3000);
 }
+
 function resetUploadButton() {
   const uploadButton = document.getElementById("upload-button");
-  uploadButton.disabled = true; // Disable the button after upload or if no file is selected
-  fileInfoDisplay.textContent = ""; // Clear the file info text
-  document.getElementById("file-input").value = ""; // Clear the file input
+  uploadButton.disabled = true;
+  fileInfoDisplay.textContent = "";
+  document.getElementById("file-input").value = "";
   resetUploadDisplay();
 }
 
@@ -145,7 +152,9 @@ window.onload = function () {
   fileInput.addEventListener("change", handleFileSelect);
 
   uploadButton.disabled = true;
-  uploadButton.addEventListener("click", uploadFile);
+  uploadButton.addEventListener("click", function() {
+    uploadFile(fileInput.files[0]);
+  });
 
   // Chart Initialization
   var ctx = document.getElementById("speedChart").getContext("2d");
