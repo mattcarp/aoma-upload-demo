e0821cf7f8cc11dbbc5c005306e614ef520c0c1a
mcarpent007 <60732871+mcarpent007@users.noreply.github.com>
2023-12-01 04:17:54 +0100

    working with chunking

--- public/js/upload.js
+++ public/js/upload.js
@@ -180,6 +180,108 @@ window.onload = function () {
   });
 
   // Chart Initialization
+  const data = {
+    datasets: [{
+      label: 'Dataset 1',
+      data: [/* Your data points */],
+      borderColor: '#2a41a1', // Graph Line Color 1
+      backgroundColor: 'rgba(42, 65, 161, 0.5)', // Translucent version of Graph Line Color 1
+      fill: false,
+      lineTension: 0.1, // Adjust line tension to your preference (0 for no bezier curves)
+      pointBackgroundColor: '#fff', // Color for the points
+      pointBorderColor: '#2a41a1', // Border color for the points
+      pointHoverBackgroundColor: '#2a41a1', // Hover color for the points
+      pointHoverBorderColor: '#fff', // Hover border color for the points
+    }, {
+      label: 'Dataset 2',
+      data: [/* Your data points */],
+      borderColor: '#2b3e9c', // Graph Line Color 2
+      backgroundColor: 'rgba(43, 62, 156, 0.5)', // Translucent version of Graph Line Color 2
+      fill: false,
+      lineTension: 0.1,
+      pointBackgroundColor: '#fff',
+      pointBorderColor: '#2b3e9c',
+      pointHoverBackgroundColor: '#2b3e9c',
+      pointHoverBorderColor: '#fff',
+    }]
+  };
+  
+  const options = {
+    scales: {
+      yAxes: [{
+        type: 'linear',
+        display: true,
+        position: 'left',
+        scaleLabel: {
+          display: true,
+          labelString: 'Speed (Mbps)'
+        },
+        gridLines: {
+          color: '#372d31', // Grid Line Color
+          lineWidth: 1,
+          zeroLineColor: '#372d31',
+          zeroLineWidth: 1,
+          drawBorder: true,
+        },
+        ticks: {
+          min: 450, // Start at 450 Mbps
+          stepSize: 50, // Adjust this value as needed for your scale steps
+          fontColor: 'white' // Assuming a light font color for visibility against the dark background
+        }
+      }],
+      xAxes: [{
+        gridLines: {
+          color: '#372d31', // Grid Line Color
+          lineWidth: 1,
+          zeroLineColor: '#372d31',
+          zeroLineWidth: 1,
+          drawBorder: true,
+        },
+        ticks: {
+          fontColor: 'white' // Assuming a light font color for visibility against the dark background
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Time',
+          fontColor: 'white'
+        }
+      }]
+    },
+    legend: {
+      display: true,
+      position: 'top',
+      labels: {
+        fontColor: 'white', // Assuming a light font color for visibility against the dark background
+        boxWidth: 20,
+      }
+    },
+    responsive: true,
+    maintainAspectRatio: false, // Ensures the chart size is responsive
+    maintainAspectRatio: false,
+    animation: {
+      duration: 1000, // Animation duration in milliseconds
+    },
+    hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
+    elements: {
+      line: {
+        tension: 0.4 // Smoothes out the line
+      }
+    }
+  };
+  
+  // Then you would initialize your chart with these options
+  // For example:
+  // new Chart(ctx, { type: 'line', data: data, options: options });
+  
+
+
   var ctx = document.getElementById("speedChart").getContext("2d");
   speedChart = new Chart(ctx, {
     type: "line",
@@ -188,8 +290,8 @@ window.onload = function () {
       datasets: [
         {
           label: "Upload Speed (KB/s)",
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          borderColor: "rgba(255, 99, 132, 1)",
+          backgroundColor: "rgba(0, 123, 255, 0.2)", // Example new background color
+          borderColor: "rgba(0, 123, 255, 1)", // Example new border color
           data: [],
         },
       ],
@@ -206,3 +308,39 @@ window.onload = function () {
   // Initially hide the speed chart
   document.getElementById("speedChart").style.display = "none";
 };
+
+document.getElementById('chunkSizeSlider').addEventListener('input', function() {
+    const chunkSize = this.value * 1024 * 1024; // Convert MB to bytes
+    document.getElementById('chunkSizeDisplay').textContent = this.value + ' MB';
+    console.log('Chunk size set to:', chunkSize, 'bytes');
+    // Store chunkSize in a global variable or use it directly in the upload function
+});
+
+async function uploadFile(file) {
+    const chunkSize = 5; // placehoder value to avoid errors
+    const totalChunks = Math.ceil(file.size / chunkSize);
+    document.getElementById('totalChunks').textContent = totalChunks;
+
+    for (let i = 0; i < totalChunks; i++) {
+        const chunk = file.slice(i * chunkSize, (i + 1) * chunkSize);
+        await uploadChunk(chunk, i, totalChunks); // Call the chunk upload function
+    }
+}
+
+async function uploadChunk(chunk, chunkNumber, totalChunks) {
+    // Implement the logic to upload the chunk
+    // Update the UI based on success or failure of each chunk upload
+    // If a chunk upload fails, add it to the failed chunks list and provide a retry option
+}
+
+function updateChunkInfo(uploadedChunks, totalChunks) {
+    document.getElementById('uploadedChunks').textContent = uploadedChunks;
+}
+
+function updateFailedChunksList(failedChunks) {
+    // Update the list of failed chunks with retry options
+}
+
+function updateProgressBar(progressPercentage) {
+    // Update the progress bar based on the current upload status
+}
