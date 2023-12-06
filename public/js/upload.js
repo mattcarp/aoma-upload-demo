7aeb8427eae4e10f4f39d429d89e057168f1ec74
mcarpent007 <60732871+mcarpent007@users.noreply.github.com>
2023-12-06 21:36:49 +0100

    we gettin there. wiped all css, major refactor to the js

--- public/js/upload.js
+++ public/js/upload.js
@@ -12,7 +12,6 @@ async function getSignedUrl(filename, contentType) {
     );
     if (response.ok) {
       const data = await response.json();
-      console.log("signed url, from within getSignedUrl: ", data.signedUrl);
       return data.signedUrl;
     } else {
       console.error(
@@ -28,125 +27,216 @@ async function getSignedUrl(filename, contentType) {
   }
 }
 
-fetch("/api/speedtest")
-  .then((response) => response.json())
-  .then((data) => {
-    displaySpeed(data.speed);
-  })
-  .catch((error) => console.error("Error fetching the speed:", error));
-
-  function displaySpeed(speed) {
-    const speedElement = document.getElementById("internet-speed-value"); // Updated ID
-    speedElement.textContent = `${speed.toFixed(4)} Mbps`;
-  }
-
-  function updateSpeedChart(chart, speed) {
-    const elapsedTime = new Date().getTime() - startTime;
-    const minutes = Math.floor(elapsedTime / 60000);
-    const seconds = ((elapsedTime % 60000) / 1000).toFixed(0);
-    
-    // Format the elapsed time as mm:ss
-    const timeLabel = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-    
-    chart.data.labels.push(timeLabel);
-    chart.data.datasets.forEach((dataset) => {
-      dataset.data.push(speed);
-    });
-    chart.update();
-  }
+// Function to update speed chart
+function updateSpeedChart(chart, speed) {
+  const elapsedTime = new Date().getTime() - startTime;
+  const minutes = Math.floor(elapsedTime / 60000);
+  const seconds = ((elapsedTime % 60000) / 1000).toFixed(0);
   
+  // Format the elapsed time as mm:ss
+  const timeLabel = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  
+  chart.data.labels.push(timeLabel);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.push(speed);
+  });
+  chart.update();
+}
 
+// Function to calculate speed
 function calculateSpeed(bytesUploaded, startTime) {
   const duration = (new Date().getTime() - startTime) / 1000;
   return bytesUploaded / duration / 1024;
 }
 
-async function uploadFile(file) {
-  console.log("called uploadFile:", file.name, file.type);
 
-  const url = await getSignedUrl(file.name, file.type);
-  if (!url) {
-    console.error("No signed URL returned.");
-    return;
-  }
-    // Record the start time when the upload starts
-    startTime = new Date().getTime();
+// Function to display speed
+function displaySpeed(speed) {
+  const speedElement = document.getElementById("internet-speed-value"); // Updated ID
+  speedElement.textContent = `${speed.toFixed(4)} Mbps`;
+}
 
-  const xhr = new XMLHttpRequest();
-  xhr.open("PUT", url, true);
-  xhr.setRequestHeader("Content-Type", file.type);
+// KEEP this one, delete the other two DOMContentLoaded funcs
+document.addEventListener("DOMContentLoaded", function() {
+  // Initialize dragDropArea and fileInfoDisplay
+  dragDropArea = document.getElementById("drag-drop-area");
+  fileInfoDisplay = document.getElementById("selected-file-name");
 
-  xhr.upload.onprogress = function (e) {
-    if (e.lengthComputable) {
-      const percentComplete = (e.loaded / e.total) * 100;
-      document.getElementById("percent-complete-value").innerText = percentComplete.toFixed(2) + "%"; // Updated ID
-  
-      // Calculate the speed using the calculateSpeed function
-      const speed = calculateSpeed(e.loaded, new Date().getTime());
-      document.getElementById("internet-speed-value").innerText = speed.toFixed(2) + " KB/s"; // Updated ID
-      
-      // Update the speed chart with the new speed value
-      // This will invoke the function updateSpeedChart passing in the chart instance and the new speed value
-      updateSpeedChart(speedChart, speed);
+  // Add event listeners for drag and drop events
+  dragDropArea.addEventListener("dragover", handleDragOver);
+  dragDropArea.addEventListener("drop", handleFileDrop);
+  dragDropArea.addEventListener("dragleave", resetDragDropArea);
+  dragDropArea.addEventListener("dragend", resetDragDropArea);
+
+  // Add event listener for click event
+  console.log("Adding click event listener to drag and drop area.");
+  dragDropArea.addEventListener("click", function () {
+    event.stopPropagation();
+    const fileInput = document.getElementById("file-input");
+    console.log("File input element:", fileInput); // Log the file input element
+    fileInput.click();
+  });
+
+  // Add event listener for file input change event
+  document.getElementById("file-input").addEventListener("change", handleFileSelect);
+
+  // Speed Chart Initialization with an Empty Dataset
+  const speedCtx = document.getElementById("speedChart").getContext("2d");
+  speedChart = new Chart(speedCtx, {
+    type: 'line',
+    data: {
+      labels: [], // Start with an empty array for timestamps
+      datasets: [{
+        label: 'Upload Speed (KB/s)',
+        data: [], // Start with an empty array for speed data
+        backgroundColor: 'rgba(0, 123, 255, 0.2)',
+        borderColor: 'rgba(0, 123, 255, 1)',
+        fill: false,
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        }
+      },
+      plugins: {
+        legend: {
+          display: true,
+        }
+      },
+      responsive: true,
+      maintainAspectRatio: true,
     }
-  };
-  
+  });
 
+  // Fetch speed test data
+  fetch("/api/speedtest")
+    .then((response) => response.json())
+    .then((data) => {
+      displaySpeed(data.speed);
+    })
+    .catch((error) => console.error("Error fetching the speed:", error));
 
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      console.log("File uploaded successfully");
+  // Initialize the doughnut chart
+  const ctx = document.getElementById('completionDonut').getContext('2d');
+  completionDonut = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+        data: [0, 100], // Start with 0% complete
+        backgroundColor: ['green', 'lightgray']
+      }]
+    },
+    options: {
+      circumference: Math.PI,
+      rotation: -Math.PI,
+      cutout: '80%',
+      tooltips: { enabled: false },
+      hover: { mode: null },
+      responsive: true,
+      maintainAspectRatio: true,
+    }
+  });
+
+  document.getElementById("upload-button").addEventListener("click", function () {
+    const fileInput = document.getElementById("file-input");
+    if (fileInput.files.length > 0) {
+      console.log("Upload button clicked. Starting upload..."); // Log when the upload button is clicked
+      resetUploadDisplay();
+      uploadFile(fileInput.files[0]); // Start the upload when the upload button is clicked
     } else {
-      console.error("Upload failed:", xhr.responseText);
+      console.log("No file selected. Please select a file to upload."); // Log when no file is selected
     }
-  };
+  });
+});
 
-  xhr.onerror = function () {
-    console.error("Error during the upload process.");
-  };
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      console.log("Upload complete");
-      resetUploadButton();
+function uploadFile(file) {
+  console.log(`Uploading file: ${file.name}`); // Log the name of the file being uploaded
+  getSignedUrl(file.name, file.type).then(url => {
+    if (!url) {
+      console.error("Failed to get a signed URL for the file upload.");
+      updateUploadStatus("Failed to start the upload. Please try again."); // Update the upload status
+      return;
     }
-  };
 
-  console.log("about to send file");
-  xhr.send(file);
-  
+    startTime = new Date().getTime();
+    const xhr = new XMLHttpRequest();
 
-  function handleDragOver(e) {
-    e.preventDefault();
-    dragDropArea.classList.add("active");
+    xhr.upload.addEventListener("progress", function (e) {
+      if (e.lengthComputable) {
+        const percentComplete = (e.loaded / e.total) * 100;
+        // Update the doughnut chart
+        completionDonut.data.datasets[0].data = [percentComplete, 100 - percentComplete];
+        completionDonut.update();
+        const speed = calculateSpeed(e.loaded, startTime);
+        updateSpeedChart(speedChart, speed);
+      }
+    });
+
+    xhr.addEventListener("load", function () {
+      if (xhr.status === 200) {
+        console.log("Upload complete!");
+      } else {
+        console.error(`Upload failed: ${xhr.status} ${xhr.statusText}`);
+      }
+    });
+
+    xhr.addEventListener("error", function () {
+      console.error(`Upload failed: ${xhr.status} ${xhr.statusText}`);
+    });
+
+    xhr.open("PUT", url, true);
+    xhr.setRequestHeader("Content-Type", file.type);
+    xhr.send(file);
+  });
+}
+
+function updateUploadStatus(message) {
+  const statusElement = document.getElementById("upload-status");
+  statusElement.textContent = message;
+}
+
+function handleDragOver(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  if (!dragDropArea.classList.contains("dragging")) {
+    dragDropArea.classList.add("dragging");
   }
+}
 
-  function handleFileDrop(e) {
-    e.preventDefault();
-    const files = e.dataTransfer.files;
-    displayFileInfo(files[0]);
-    resetDragDropArea();
+function handleFileDrop(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  if (dragDropArea.classList.contains("dragging")) {
+    dragDropArea.classList.remove("dragging");
   }
-
+  const files = e.dataTransfer.files;
+  if (files.length > 0) {
+    displayFileInfo(files[0]); // Display the file info instead of starting the upload
+  }
+}
 
 function resetDragDropArea() {
-  dragDropArea.classList.remove("active");
+  if (dragDropArea.classList.contains("dragging")) {
+    dragDropArea.classList.remove("dragging");
+  }
 }
 
 function handleFileSelect(e) {
-  const file = e.target.files[0];
-  displayFileInfo(file);
-  document.getElementById("upload-button").disabled = !file;
-  resetUploadDisplay();
+  const files = e.target.files;
+  if (files.length > 0) {
+    displayFileInfo(files[0]); // Display the file info instead of starting the upload
+  }
 }
 
 function displayFileInfo(file) {
-  const selectedFileDisplay = document.getElementById("selected-file-value");
-  if (file) {
-    selectedFileDisplay.textContent = file.name;
-  } else {
-    selectedFileDisplay.textContent = "No file chosen";
-  }
+  fileInfoDisplay.textContent = `Selected file: ${file.name}, ${file.size} bytes`;
+  console.log(`File selected: ${file.name}, ${file.size} bytes`); // Log the selected file info
+
+    // Enable the upload button
+    const uploadButton = document.getElementById("upload-button");
+    uploadButton.disabled = false;
 }
 
 function triggerFileSelect() {
@@ -154,167 +244,12 @@ function triggerFileSelect() {
 }
 
 function resetUploadDisplay() {
-  const uploadPercentage = document.getElementById("percent-complete-value");
-  const uploadSpeed = document.getElementById("internet-speed-value");
-
-  // temp comment while bug fixing
-  // uploadPercentage.style.opacity = "0";
-  // uploadSpeed.style.opacity = "0";
-
-  setTimeout(() => {
-    uploadPercentage.style.opacity = "1";
-    uploadSpeed.style.opacity = "1";
-  }, 3000);
-}
-
-function resetUploadButton() {
-  const uploadButton = document.getElementById("upload-button");
-  uploadButton.disabled = true;
-  fileInfoDisplay.textContent = "";
-  document.getElementById("file-input").value = "";
-  resetUploadDisplay();
-}
-
-window.onload = function () {
-  dragDropArea = document.getElementById("drag-drop-area");
-  dragDropArea.addEventListener("click", triggerFileSelect);
-  dragDropArea.addEventListener("dragover", handleDragOver);
-  dragDropArea.addEventListener("drop", handleFileDrop);
-  dragDropArea.addEventListener("dragleave", resetDragDropArea);
-
-  fileInfoDisplay = document.getElementById("selected-file-value"); // Ensure this ID is correct.
-
-  const fileInput = document.getElementById("file-input");
-  fileInput.addEventListener("change", handleFileSelect);
-
-  const uploadButton = document.getElementById("upload-button");
-  uploadButton.addEventListener("click", function () {
-      console.log("Upload button clicked", fileInput.files[0]);
-      uploadFile(fileInput.files[0]);
+  document.getElementById("percent-complete-value").textContent = "0%";
+  // this seems to no longer be in the html:
+  // document.getElementById("upload-status").textContent = "";
+  speedChart.data.labels = [];
+  speedChart.data.datasets.forEach((dataset) => {
+    dataset.data = [];
   });
-
- // Speed Chart Initialization with an Empty Dataset
-const speedCtx = document.getElementById("speedChart").getContext("2d");
-speedChart = new Chart(speedCtx, {
-  type: 'line',
-  data: {
-    labels: [], // Start with an empty array for timestamps
-    datasets: [{
-      label: 'Upload Speed (KB/s)',
-      data: [], // Start with an empty array for speed data
-      backgroundColor: 'rgba(0, 123, 255, 0.2)',
-      borderColor: 'rgba(0, 123, 255, 1)',
-      fill: false,
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      }
-    },
-    plugins: {
-      legend: {
-        display: true,
-      }
-    },
-    responsive: true,
-    maintainAspectRatio: true,
-  }
-});
-
-
-  // Donut Chart Initializations with Static Data
-  const completionCtx = document.getElementById('completionDonut').getContext('2d');
-  const completionChart = new Chart(completionCtx, {
-      type: 'doughnut',
-      data: {
-          labels: ['Complete', 'Incomplete'], // Example labels
-          datasets: [{
-              data: [70, 30], // Static data for testing
-              backgroundColor: [
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(192, 75, 75, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(192, 75, 75, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
-              legend: {
-                  display: true,
-              }
-          }
-      }
-  });
-
-  const speedDonutCtx = document.getElementById('speedDonut').getContext('2d');
-  const speedDonutChart = new Chart(speedDonutCtx, {
-      type: 'doughnut',
-      data: {
-          labels: ['Fast', 'Slow'], // Example labels
-          datasets: [{
-              data: [65, 35], // Static data for testing
-              backgroundColor: [
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
-              legend: {
-                  display: true,
-              }
-          }
-      }
-  });
-};
-
-
-
-
-document.getElementById('chunkSizeSlider').addEventListener('input', function() {
-  const chunkSize = this.value * 1024 * 1024;
-  document.getElementById('chunkSizeDisplay').textContent = this.value + ' MB';
-  console.log('Chunk size set to:', chunkSize, 'bytes');
-});
-
-let isPaused = false;
-let lastUploadedChunkIndex = 0;
-
-document.getElementById('pause-button').addEventListener('click', function() {
-  isPaused = true;
-});
-
-document.getElementById('resume-button').addEventListener('click', function() {
-  isPaused = false;
-  if (fileInput.files.length > 0) {
-    continueUploading(fileInput.files[0], lastUploadedChunkIndex);
-  }
-});
-
-function continueUploading(file, startIndex) {
-  const chunkSize = 5 * 1024 * 1024;
-  const totalChunks = Math.ceil(file.size / chunkSize);
-  for (let i = startIndex; i < totalChunks; i++) {
-    if (isPaused) {
-      lastUploadedChunkIndex = i;
-      break;
-    }
-  }
-}
+  speedChart.update();
 }
