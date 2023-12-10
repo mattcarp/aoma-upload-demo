20e4fcbe7e31ca56e84774e2e75a9db2035e3ce1
mcarpent007 <60732871+mcarpent007@users.noreply.github.com>
2023-12-10 17:44:08 +0100

    set up for es6 modules, moving to v3 of aws

--- server/server.js
+++ server/server.js
@@ -1,33 +1,51 @@
-require('dotenv').config();
-const express = require('express');
-const AWS = require('aws-sdk');
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import FastSpeedtest from "fast-speedtest-api";
+import { S3 } from '@aws-sdk/client-s3'
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+
 const app = express();
 
 console.log("hello from the server");
 
-const FastSpeedtest = require("fast-speedtest-api");
+// const FastSpeedtest = import("fast-speedtest-api");
 
-let speedtest = new FastSpeedtest({
-  token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", // Replace with the actual token
-  verbose: false, // Let's keep it simple
-  timeout: 10000, // 10 seconds should do
-  https: true, // Secure, as we like it
-  urlCount: 5, // Default magic number
-  bufferSize: 8, // Buffer size for the speedtest
-  unit: FastSpeedtest.UNITS.Mbps // Speed in Mbps, because we're civilized
-});
+// let speedtest = new FastSpeedtest({
+//   token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", // Replace with the actual token
+//   verbose: false, // Let's keep it simple
+//   timeout: 10000, // 10 seconds should do
+//   https: true, // Secure, as we like it
+//   urlCount: 5, // Default magic number
+//   bufferSize: 8, // Buffer size for the speedtest
+//   unit: FastSpeedtest.UNITS.Mbps // Speed in Mbps, because we're civilized
+// });
 
-speedtest.getSpeed().then(speed => {
-  console.log(`Your internet speed is: ${speed} Mbps. Lightning fast, isn't it?`);
-}).catch(e => {
-  console.error(`Error: ${e.message}. Maybe the hamsters powering your internet took a break.`);
-});
 
-function getInternetSpeed() {
-    return speedtest.getSpeed().then(speed => speed).catch(e => console.error(e.message));
+
+async function getInternetSpeed() {
+  let speedtest = new FastSpeedtest({
+    token: process.env.FAST_SPEEDTEST_API_TOKEN,
+    verbose: false,
+    timeout: 10000,
+    https: true,
+    urlCount: 5,
+    bufferSize: 8,
+    unit: FastSpeedtest.UNITS.Mbps
+  });
+
+  try {
+    const speed = await speedtest.getSpeed();
+    console.log(`Internet speed: ${speed} Mbps`);
+    return speed;
+  } catch (err) {
+    console.error(`Error getting internet speed: ${err.message}`);
+    return null;
+  }
 }
 
-module.exports = { getInternetSpeed };
+export { getInternetSpeed };
 
 // Define a route to fetch internet speed
 app.get('/api/speedtest', async (req, res) => {
@@ -44,34 +62,30 @@ app.get('/api/speedtest', async (req, res) => {
 app.use(express.static('public'));
 
 // Configure AWS with environment variables
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'us-east-1'
+const s3Client = new S3({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 });
-console.log("AWS.config: ", AWS.config);
 
-const s3 = new AWS.S3();
-
-app.get('/generate-signed-url', (req, res) => {
-  console.log("called generate-signed-url");
+app.get('/generate-signed-url', async (req, res) => {
+  const { filename, contentType } = req.query;
   const params = {
-    Bucket: 'aoma-demo-bucket',
-    Key: req.query.filename,
-    Expires: 1800, // The URL will expire in 1800 seconds
-	ContentType: req.query.contentType
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: filename,
+    ContentType: contentType,
   };
 
-  console.log("params: ", params);
-
-  s3.getSignedUrl('putObject', params, (err, url) => {
-    if (err) {
-      console.error('Error generating signed URL', err);
-      return res.status(500).send(err);
-    }
-	console.log('Generated signed URL:', url);
-    res.json({ signedUrl: url });
-  });
+  try {
+    const command = new PutObjectCommand(params);
+    const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+    res.json({ signedUrl });
+  } catch (err) {
+    console.error('Error generating signed URL', err);
+    res.status(500).send('Error generating signed URL');
+  }
 });
 
 const PORT = process.env.PORT || 3000;
