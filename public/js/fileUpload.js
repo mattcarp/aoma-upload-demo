452265f46c2b292bf73149593770b16e25a85d4d
mcarpent007 <60732871+mcarpent007@users.noreply.github.com>
2023-12-11 14:05:26 +0100

    all things check out logically. will do ui tests now

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -1,10 +1,15 @@
-import { getSignedUrlForChunk } from './s3UtilityFunctions.js';
-import * as DomUtils from './domInteraction.js';
-import { showUploadComplete } from './domInteraction.js'; // Adjust the path if needed
-
+import { getSignedUrlForChunk } from "./s3UtilityFunctions.js";
+import {
+  resetUploadDisplay,
+  updateUploadProgress,
+  showUploadError,
+  showUploadComplete,
+  updatePercentComplete,
+  updateUploadSpeed,
+  updateUploadedChunks,
+} from "./domInteraction.js";
 
 const CHUNK_SIZE = 5 * 1024 * 1024; // 5 megabytes in bytes
-
 
 // Define splitFileIntoChunks function
 function splitFileIntoChunks(file, chunkSize) {
@@ -18,58 +23,6 @@ function splitFileIntoChunks(file, chunkSize) {
   return chunks;
 }
 
-// Main upload function
-// export async function uploadFile(file, completionDonut) {
-//   DomUtils.resetUploadDisplay();
-//   console.log(`Uploading file: ${file.name}`);
-
-//   // Initiate multipart upload and get uploadId
-//   const uploadIdResponse = await fetch(`/start-multipart-upload`, {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify({ filename: file.name, contentType: file.type })
-//   });
-//   const uploadIdData = await uploadIdResponse.json();
-//   const uploadId = uploadIdData.uploadId;
-
-//   // Split file into chunks
-//   const chunks = splitFileIntoChunks(file, CHUNK_SIZE);
-
-//   // Upload each chunk
-//   for (const [index, chunk] of chunks.entries()) {
-//     const partNumber = index + 1;
-//     try {
-//       const signedUrl = await getSignedUrlForChunk(file.name, partNumber, uploadId);
-//       const response = await fetch(signedUrl, {
-//         method: 'PUT',
-//         body: chunk,
-//       });
-//       if (!response.ok) {
-//         throw new Error(`Chunk ${partNumber} upload failed`);
-//       }
-//       // Update UI for successful chunk upload
-//       // (Update progress, completionDonut, etc.)
-//     } catch (error) {
-//       console.error('Error uploading chunk:', error);
-//       // Handle chunk upload error (e.g., retry, skip, abort)
-//       // Update UI to reflect the error
-//     }
-//   }
-
-//   // Complete the multipart upload
-//   try {
-//     await fetch(`/complete-multipart-upload`, {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({ filename: file.name, uploadId })
-//     });
-//     // Handle successful completion (e.g., update UI)
-//   } catch (error) {
-//     console.error('Error completing upload:', error);
-//     // Update UI to reflect the error
-//   }
-// }
-
 // Assuming you have other necessary imports and utility functions like getSignedUrlForChunk
 
 export async function uploadFile(file, completionDonut) {
@@ -77,79 +30,69 @@ export async function uploadFile(file, completionDonut) {
   console.log(`Uploading file: ${file.name}`);
 
   // Step 1: Initiate multipart upload and get uploadId
-  const initiateResponse = await fetch('/start-multipart-upload', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ filename: file.name, contentType: file.type })
+  const initiateResponse = await fetch("/start-multipart-upload", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ filename: file.name, contentType: file.type }),
   });
-  const { uploadId } = await initiateResponse.json();
 
-  // Split the file into chunks
-  const chunks = splitFileIntoChunks(file, CHUNK_SIZE);
-
-  // Initialize an array to store the part numbers and ETags
-  const parts = [];
+  const chunks = splitFileIntoChunks(file); // Split the file into chunks
+  const uploadId = await startMultipartUpload(file); // Start the multipart upload to get the uploadId
+  const parts = []; // Initialize the parts array to store upload details for each chunk
 
   // Upload each chunk
-  // ...
+  for (const [index, chunk] of chunks.entries()) for (const [index, chunk] of chunks.entries()) {
+    const partNumber = index + 1;
+    try {
+      const signedUrl = await getSignedUrlForChunk(file.name, partNumber, uploadId); // Get signed URL for the chunk
 
-// Upload each chunk
-for (const [index, chunk] of chunks.entries()) {
-  const partNumber = index + 1;
-  try {
-    // Get the signed URL for each chunk
-    const signedUrlResponse = await fetch('/get-signed-url-for-chunk', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ fileName: file.name, partNumber, uploadId })
-    });
-    const { signedUrl } = await signedUrlResponse.json();
+      const uploadResponse = await fetch(signedUrl, {
+        method: 'PUT',
+        body: chunk,
+        headers: {
+          'Content-Type': 'binary/octet-stream', // or your specific content type
+        },
+      });
 
-    // Upload the chunk to S3
-    const uploadResponse = await fetch(signedUrl, {
-      method: 'PUT',
-      body: chunk
-    });
+      if (!uploadResponse.ok) {
+        throw new Error(`HTTP error! status: ${uploadResponse.status}`);
+      }
 
-    if (!uploadResponse.ok) {
-      throw new Error(`HTTP error! status: ${uploadResponse.status}`);
-    } else {
       const etag = uploadResponse.headers.get('ETag');
       if (!etag) {
         throw new Error('ETag not found in the response');
       }
-      parts.push({ ETag: etag, PartNumber: partNumber });
-      // Update the UI or completionDonut with the progress
-      DomUtils.updateUploadProgress(partNumber, chunks.length);
+
+      parts.push({ ETag: etag, PartNumber: partNumber }); // Store the ETag and part number for each uploaded chunk
+
+      const percentComplete = Math.round((partNumber / chunks.length) * 100);
+      updatePercentComplete(percentComplete); // Update the UI with the percent complete
+
+      updateUploadProgress(partNumber, chunks.length); // Update the UI with the upload progress
+      updateUploadedChunks(`Uploaded part ${partNumber} of ${chunks.length}`);
+
+    } catch (error) {
+      console.error(`Error uploading chunk number ${partNumber}:`, error);
+      showUploadError(error); // Show error in the UI
+      return; // Exit the upload process
     }
-  } catch (error) {
-    console.error(`Error uploading chunk number ${partNumber}:`, error);
-    // Handle the chunk upload error
-    DomUtils.showUploadError(error);
-    return;
   }
-}
-
-// ...
-
-
+ 
 
   // Complete the multipart upload
   try {
-    const completionResponse = await fetch('/complete-multipart-upload', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ filename: file.name, uploadId, parts }) // Send the parts array
+    const completionResponse = await fetch("/complete-multipart-upload", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ filename: file.name, uploadId, parts }), // Send the parts array
     });
     const completionData = await completionResponse.json();
-    console.log('Multipart upload completed:', completionData);
+    console.log("Multipart upload completed:", completionData);
     // Update the UI or completionDonut for completion
-    DomUtils.showUploadComplete(completionData);
+    showUploadComplete(completionData);
   } catch (error) {
-    console.error('Error completing multipart upload:', error);
+    console.error("Error completing multipart upload:", error);
     // Handle the completion error
-    DomUtils.showUploadError(error);
+    showUploadError(error);
   }
 }
-
-
